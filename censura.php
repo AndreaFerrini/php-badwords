7c0d8cacc77bcfc8cc3516cd0129872e13283237
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-05-20 12:02:17 +0200

    creazione Form con testo e censure

--- censura.php
+++ censura.php
@@ -1,14 +1,22 @@
+<?php
+
+$testo = $_GET['testo'];
+$parola = $_GET['censure'];
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saluto php</title>
+    <title>Censura php</title>
 </head>
 <body>
     
-    <h2>Questa è la pagina per salutarti</h2>
+    <h2>Questa è la pagina di censura Badwords</h2>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 $name = "Andrea";
 
-$nomeParametro = $_GET["name"]; //And
-$cognomeParametro = $_GET["cognome"]; //Fer
-
-echo $nomeparametro . " "  . $cognomeParametro;
-
 ?>
 
 
@@ -17,8 +12,30 @@ echo $nomeparametro . " "  . $cognomeParametro;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BadWords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Ciao mi chiamo <?php echo $name ?></h1>
+    <h1>Ciao mi chiamo <?php echo $name ?> e qui puoi scrivere il tuo testo con le parole da censurare</h1>
+
+    <form action="censura.php" method="GET">
+  
+        <div class="m-4">
+               <label for="exampleFormControlTextarea1" class="form-label">Inserisci testo</label>
+               <input class="form-control form-control-lg" type="text" placeholder="..." aria-label=".form-control-lg example" name="testo">
+        </div>
+
+        <div class="m-4">
+               <label for="exampleFormControlTextarea1" class="form-label">Scegli una parola da censurare</label>
+               <input class="form-control form-control-lg" type="text" placeholder="..." aria-label=".form-control-lg example" name="censure">
+        </div>
+
+        <div class="text-center">
+               <button type="submit" class="btn btn-outline-success text-center">Invia dati</button>
+               <input class="btn btn-primary" type="submit" value="Submit">
+        </div>
+
+
+    </form>
+
 </body>
 </html>
